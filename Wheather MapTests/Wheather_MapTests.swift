af11165b2afe6ef7bcedc623a912dce539136eb5
Yana Sychevskaya <sychevskaya404@gmail.com>
2020-08-11 19:30:07 +0300

    Initial Commit

---
+++ Wheather MapTests/Wheather_MapTests.swift
@@ -0,0 +1,33 @@
+//
+//  Wheather_MapTests.swift
+//  Wheather MapTests
+//
+//  Created by Yana on 11.08.2020.
+//
+
+import XCTest
+@testable import Wheather_Map
+
+class Wheather_MapTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
